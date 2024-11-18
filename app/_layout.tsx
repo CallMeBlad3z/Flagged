75555bf5b2f793d8399e4fa93867cc5188a8a438
Blad3z <51128241+CallMeBlad3z@users.noreply.github.com>
2024-11-18 19:51:01 +0200

    Add navigation links and country list screen to RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,71 @@
-import { Stack } from "expo-router";
+import { Stack, Link } from "expo-router";
+import { Button, View, StyleSheet } from "react-native";
+import { SelectedCountriesProvider } from "./components/api/SelectedCountriesContext";
+import { Ionicons, AntDesign } from "@expo/vector-icons";
+
 
 export default function RootLayout() {
+
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: 'Home' }} />
-      <Stack.Screen name="settings" options={{ title: 'Settings' }} />
-    </Stack>
+    <SelectedCountriesProvider>
+      <Stack>
+        <Stack.Screen 
+        name="index" 
+        options={{
+          title: 'flagged.',
+          headerRight: () => (
+            <View style={styles.navbar}>
+              <Link style={styles.navLink} href="/settings">
+                <View style={styles.iconWrapper}>
+                    <Ionicons name="settings" size={24} color="black" />
+                </View>
+              </Link>
+              <Link style={styles.navLink} href="countrylist">
+                <View style={styles.iconWrapper}>
+                    <AntDesign name="plus" size={24} color="black" />
+                </View>
+              </Link>
+            </View>
+          ),
+        }} />
+        <Stack.Screen name="settings" options={{ title: 'Settings' }} />
+        <Stack.Screen
+          name="countrylist"
+          options={{
+            title: 'Country List',
+            headerRight: () => (
+              <Button
+                title="Save"
+                color="#007BFF"
+              />
+            ),
+          }}
+        />
+      </Stack>
+    </SelectedCountriesProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  navbar: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 10,
+  },
+  iconContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  navLink: {
+    marginLeft: 15, // Add space between the icons
+  },
+  iconWrapper: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: "#e0e0e0",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
