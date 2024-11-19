bb84e4c2c741904458d7b7c5be21289add67b45c
Blad3z <51128241+CallMeBlad3z@users.noreply.github.com>
2024-11-20 01:17:53 +0200

    Update MapComponent to use new style URL and disable user interactions

--- app/components/map.tsx
+++ app/components/map.tsx
@@ -1,16 +1,26 @@
 // components/map.tsx
 
 import { View, StyleSheet } from 'react-native';
-import { MapView } from '@rnmapbox/maps';
+import { MapView, Camera } from '@rnmapbox/maps';
 import '@/mapboxConfig'; // Adjust the path as needed
 
 const MapComponent = () => {
   return (
     <View style={styles.container}>
       <MapView
-        styleURL="mapbox://styles/blad3z/cm2tk4cc100f501pk0grxgkt0"
+        styleURL="mapbox://styles/blad3z/cm3op886700sc01s92a29489b"
         style={styles.map}
+        scrollEnabled={false}
+        zoomEnabled={false}
+        rotateEnabled={false}
+        pitchEnabled={false}
+        logoEnabled={false} // Disable Mapbox watermark
+        attributionEnabled={false} // Disable attribution
       >
+        <Camera
+          zoomLevel={-4}
+          centerCoordinate={[21, -12]}
+        />
       </MapView>
     </View>
   );
