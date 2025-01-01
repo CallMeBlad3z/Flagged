9092a7e7522f2695b6cb4ace6229f627e080ba6d
Blad3z <51128241+CallMeBlad3z@users.noreply.github.com>
2025-01-01 23:53:40 +0200

    Enhance SelectedCountriesContext to load and manage country data; implement data fetching and local storage for improved functionality

--- app/components/api/SelectedCountriesContext.tsx
+++ app/components/api/SelectedCountriesContext.tsx
@@ -2,12 +2,20 @@
 
 import { createContext, useState, useContext, useEffect, ReactNode } from 'react';
 import * as FileSystem from 'expo-file-system';
+import CountryDataFetcher from './CountryDataFetcher';
+
+interface CountryData {
+  name: string;
+  code: string;
+  continent: string;
+}
 
 interface SelectedCountriesContextProps {
   selectedCountries: string[];
   setSelectedCountries: React.Dispatch<React.SetStateAction<string[]>>;
   savedSelectedCountries: string[];
   saveSelectedCountries: () => Promise<void>;
+  countryData: CountryData[];
 }
 
 const SelectedCountriesContext = createContext<SelectedCountriesContextProps | undefined>(undefined);
@@ -16,28 +24,44 @@ interface SelectedCountriesProviderProps {
   children: ReactNode;
 }
 
-const selectedCountriesFilePath = `${FileSystem.documentDirectory}selectedCountries.json`;
+const selectedCountriesFilePath = `${FileSystem.documentDirectory}selectedCountries.json`; // Stores the selected countries locally
+const countryDataFilePath = `${FileSystem.documentDirectory}countryData.json`;             // Stores the api countries locally
 
 export const SelectedCountriesProvider = ({ children }: SelectedCountriesProviderProps) => {
-  const [selectedCountries, setSelectedCountries] = useState<string[]>([]);
-  const [savedSelectedCountries, setSavedSelectedCountries] = useState<string[]>([]);
+  const [selectedCountries, setSelectedCountries] = useState<string[]>([]);           // State to store the selected countries
+  const [savedSelectedCountries, setSavedSelectedCountries] = useState<string[]>([]); // State to store the saved selected countries
+  const [countryData, setCountryData] = useState<CountryData[]>([]);                  // State to store the country data
 
-  // Load saved selected countries from file on mount
+  // Load saved selected countries and country data from file on mount
   useEffect(() => {
-    const loadSavedSelectedCountries = async () => {
+    const loadSavedData = async () => {
       try {
-        const fileInfo = await FileSystem.getInfoAsync(selectedCountriesFilePath);
-        if (fileInfo.exists) {
+        const selectedCountriesFileInfo = await FileSystem.getInfoAsync(selectedCountriesFilePath); // Check if the file exists
+        if (selectedCountriesFileInfo.exists) {
           const fileContents = await FileSystem.readAsStringAsync(selectedCountriesFilePath);
           const storedCountries = JSON.parse(fileContents);
-          setSavedSelectedCountries(storedCountries);
+          setSavedSelectedCountries(storedCountries); // Set the saved selected countries
+          setSelectedCountries(storedCountries);      // Loads the selected countries from the file, upon app restart
+        }
+        
+        // Load the country data
+        const countryDataFileInfo = await FileSystem.getInfoAsync(countryDataFilePath);
+        if (countryDataFileInfo.exists) { 
+          const fileContents = await FileSystem.readAsStringAsync(countryDataFilePath);
+          const storedCountryData = JSON.parse(fileContents);
+          setCountryData(storedCountryData);
+          //console.log('Loaded country data from file:', storedCountryData);   // Debug log
+        } else {
+          const data = await CountryDataFetcher(); // Fetch the country data
+          setCountryData(data);
+          await FileSystem.writeAsStringAsync(countryDataFilePath, JSON.stringify(data)); // Save the country data to a file
+          //console.log('Fetched and saved country data:', data);   // Debug log
         }
       } catch (error) {
-        console.error('Error reading saved selected countries from file:', error);
+        console.error('Error reading saved data from file:', error);
       }
     };
-
-    loadSavedSelectedCountries();
+    loadSavedData();
   }, []);
 
   // Function to save selected countries
@@ -57,13 +81,14 @@ export const SelectedCountriesProvider = ({ children }: SelectedCountriesProvide
 
   return (
     <SelectedCountriesContext.Provider
-      value={{ selectedCountries, setSelectedCountries, savedSelectedCountries, saveSelectedCountries }}
+      value={{ selectedCountries, setSelectedCountries, savedSelectedCountries, saveSelectedCountries, countryData }}
     >
       {children}
     </SelectedCountriesContext.Provider>
   );
 };
 
+// Custom hook to use the selected countries context
 export const useSelectedCountries = () => {
   const context = useContext(SelectedCountriesContext);
   if (!context) {
